df99438ab1139c37cb0ed8a03c2858a78d2183bb
athulcp_ <athulcp003@gmail.com>
2021-08-06 12:07:18 +0530

    Update script1.js

--- portfolio/script1.js
+++ portfolio/script1.js
@@ -1,11 +1,17 @@
 $(document).ready(function()
 {
     $(window).scroll(function(){
-        if(this.scrollY>20){
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-    })
-})
+    });
+    $('.menu-icon').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-icon i').toggleClass("active");
+
+    });
+    
+});
